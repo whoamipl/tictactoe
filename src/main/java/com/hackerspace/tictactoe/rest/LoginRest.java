a03993587a8e9b6b03e43bf4d7920c5f634ff9a3
psuwala <psuwala@atlassian.com>
2018-08-18 00:07:44 +0200

    added rest find

--- src/main/java/com/hackerspace/tictactoe/rest/LoginRest.java
+++ src/main/java/com/hackerspace/tictactoe/rest/LoginRest.java
@@ -2,8 +2,13 @@ package com.hackerspace.tictactoe.rest;
 
 import com.hackerspace.tictactoe.business.State;
 import com.hackerspace.tictactoe.rest.model.Login;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * TODO: Document this class / interface here
@@ -18,8 +23,11 @@ public class LoginRest {
         this.state = state;
     }
 
-    @RequestMapping("/rest/login")
-    public Login login() {
-        return new Login(state.login());
+    @RequestMapping("/rest/find/{$id}")
+    public ResponseEntity<Object> find(@PathVariable(value="id") int id) {
+        if(state.find(id)){
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
